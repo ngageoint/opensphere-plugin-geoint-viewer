39d3f0a62b6e8ea0512e4644c9939caca4521be0
Kevin Schmidt <kschmidt@caci.com>
2020-08-21 09:46:47 -0600

    refactor(imports): add plugins/etc in a single entry point

---
+++ src/osngaimports.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Plugins and other source added to the NGA OpenSphere application.
+ */
+goog.module('osngaimports');
+
+goog.require('plugin.banner.BannerPlugin');
+goog.require('plugin.geopackage.GeoPackagePlugin');
+goog.require('plugin.ispy.ISpyPlugin');
+goog.require('plugin.login.LoginPlugin');
+goog.require('plugin.overpass.OverpassPlugin');
+goog.require('plugin.piwik.PiwikPlugin');
