d4ac3f84361e28455d951a81135151f224010704
Kevin Schmidt <kschmidt@caci.com>
2021-10-29 10:15:59 -0600

    feat(net): replace login handler with pre-fetch handler

--- src/osngaimports.js
+++ src/osngaimports.js
@@ -3,14 +3,12 @@
  */
 goog.declareModuleId('osngaimports');
 
-/* eslint-disable opensphere/no-unused-vars */
+import 'opensphere-nga-lib/src/plugin/gegd/gegdplugin.js';
+import 'opensphere-nga-lib/src/plugin/ispy/ispyplugin.js';
+import 'opensphere-nga-lib/src/plugin/osnga/appplugin.js';
+import 'opensphere-nga-lib/src/plugin/overpass/overpassplugin.js';
+import 'opensphere-plugin-geopackage/src/plugin/geopackage/geopackageplugin.js';
 
-import {GEGDPlugin} from 'opensphere-nga-lib/src/plugin/gegd/gegdplugin.js';
-import {ISpyPlugin} from 'opensphere-nga-lib/src/plugin/ispy/ispyplugin.js';
-import {AppPlugin} from 'opensphere-nga-lib/src/plugin/osnga/appplugin.js';
-import {OverpassPlugin} from 'opensphere-nga-lib/src/plugin/overpass/overpassplugin.js';
-import {GeoPackagePlugin} from 'opensphere-plugin-geopackage/src/plugin/geopackage/geopackageplugin.js';
-
-import {BannerPlugin} from './plugin/banner/bannerplugin.js';
-import {LoginPlugin} from './plugin/login/loginplugin.js';
-import {PiwikPlugin} from './plugin/piwik/piwikplugin.js';
+import './gv.js';
+import './plugin/banner/bannerplugin.js';
+import './plugin/piwik/piwikplugin.js';
